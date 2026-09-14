57636f58bc9ff2b4a0678bd4f5b74d2a35af84c5
Maurilla Abdul <maurillaabdul@gmail.com>
2026-09-14 19:04:59 +0700

    feat: adding education model

--- main/models.py
+++ main/models.py
@@ -24,3 +24,20 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Education(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution = models.CharField(max_length=255)
+    field_of_study = models.CharField(max_length=255)
+    description = models.TextField()
+    start_year = models.PositiveIntegerField()
+    end_year = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.degree
+
+    @property
+    def is_ongoing(self):
+        return self.end_year is None
+
